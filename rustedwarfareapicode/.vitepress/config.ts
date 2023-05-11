7baf405c61a80f7f4582dcf02ff867ee6541cdd3
LingASDJ <2735951230@qq.com>
2023-05-11 09:15:05 +0800

    添加搜索功能-Beta

--- rustedwarfareapicode/.vitepress/config.ts
+++ rustedwarfareapicode/.vitepress/config.ts
@@ -44,5 +44,29 @@ export default defineConfig({
       message: "Rosy-Code Ling Web Stuido",
       copyright: "Copyright © 2023-RW-API-Code",
     },
+
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          zh: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换'
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 })
